3fd89f2d5216479885a21fb1ab132817b6497597
DigitalM0nkey <github@terrencekuiper.com>
2018-08-23 21:56:34 -0700

    Added static to server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use('/bots/BoozeCruise', require('./controllers/bots/BoozeCruise'));
+app.use(require('./controllers/static'));
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
   console.log('Server ', process.pid ,' listening on', port);
