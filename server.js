98f39c9e622117fb13595690659319bb523d1c6b
DigitalM0nkey <github@terrencekuiper.com>
2018-08-23 20:48:51 -0700

    Change back to port 3000

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use('/bots/BoozeCruise', require('./controllers/bots/BoozeCruise'));
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
   console.log('Server ', process.pid ,' listening on', port);
 });
