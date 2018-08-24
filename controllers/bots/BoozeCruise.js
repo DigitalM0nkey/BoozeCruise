96a3eeb6107341b1dacfec396e5e419224607fb3
DigitalM0nkey <github@terrencekuiper.com>
2018-08-23 22:22:20 -0700

    added chat id

--- controllers/bots/BoozeCruise.js
+++ controllers/bots/BoozeCruise.js
@@ -5,10 +5,15 @@ var TOKEN = config.tokens.telegram.BoozeCruise;
 var b = new TelegramBot();
 b.init(TOKEN).then(function() {
   b.introduceYourself();
-  b.deleteWebhook();
-  //b.setWebhook('BoozeCruise');
+  //b.deleteWebhook();
+  b.setWebhook('BoozeCruise');
 });
 router.post('/', function (req, res, next) {
   console.log(req.body.message);
+  res.sendStatus(200);
+});
+router.get('/', function (req, res, next) {
+  b.sendMessage('510423667', 'Received Get');
+  res.json({ message: 'get ok'});
 });
 module.exports = router;
