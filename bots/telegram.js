0133ff9cbf111016a115f37c5363586fc2cb2278
DigitalM0nkey <github@terrencekuiper.com>
2018-12-06 22:56:17 -0800

    in1Minute

--- bots/telegram.js
+++ bots/telegram.js
@@ -107,6 +107,7 @@ module.exports = function() {
   bot.kick = function(channel, user) {
       var in1Minute = new Date();
       in1Minute.setTime(in1Minute.getTime() + (60 * 1000));
+      in1Minute = Math.floor(in1Minute / 1000);
       console.log(in1Minute);
       return new Promise(function (resolve, reject) {
         var url = 'https://api.telegram.org/bot' + bot.token + '/kickChatMember?chat_id=' + channel + '&user_id=' + user + 'until_date=' + in1Minute;
