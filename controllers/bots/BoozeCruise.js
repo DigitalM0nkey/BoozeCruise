46a9e4a60b155357ad01c0c365daab9b8a30f762
Terrence Kuiper <digitalm0nkey@Terrences-MacBook-Pro.local>
2019-08-02 23:41:49 -0700

    controllers/bots/BoozeCruise: Captain's Log

--- controllers/bots/BoozeCruise.js
+++ controllers/bots/BoozeCruise.js
@@ -352,7 +352,7 @@ router.post('/', function (req, res, next) {
 
             } else if (req.body.message.text.substring(0, req.body.message.text.indexOf(' ')) == "/log") {
               if (ship._id == MYSHIP) {
-                b.sendMessage('510423667', req.body.message.text.substring(req.body.message.text.indexOf(' ') + 1));
+                b.sendMessage('510423667', "Captain's Log: " + req.body.message.text.substring(req.body.message.text.indexOf(' ') + 1));
               }
             }
 
