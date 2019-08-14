40ba7602db99ecae2a2d611af4006f3be28146a8
Terrence Kuiper <digitalm0nkey@Terrences-MacBook-Pro.local>
2019-08-13 22:14:04 -0700

    mini-game/lowestHighest: mostly working.

--- mini-game/lowestHighest.js
+++ mini-game/lowestHighest.js
@@ -4,6 +4,13 @@ let player2 = 0;
 
 const roll = function numbers(player1Guess, player2Guess) {
   house = Math.floor(Math.random() * 100);
+  if (house === 0 || house === 1 || house === 2) {
+    house = Math.floor(Math.random() * 100);
+    console.log("--- Second Random ---")
+  } if (house === 0 || house === 1 || house === 2) {
+    house = Math.floor(Math.random() * 100);
+    console.log("---- THIRD RANDOM ---- \n All players receive 50 Korona")
+  }
   if (player1Guess === player2Guess) {
     console.log("\nHouse Wins!\nPLAYER DRAW\nPlayer 1 guessed: " + player1Guess + "\nPlayer 2 guessed: " + player2Guess);
   } else if (house < player1Guess && house < player2Guess) {
@@ -16,7 +23,7 @@ const roll = function numbers(player1Guess, player2Guess) {
     } else {
       console.log("\nHouse Wins!\nHouse picked: " + house + "\nPlayer 1 guessed: " + player1Guess + "\nPlayer 2 guessed: " + player2Guess);
     }
-    // console.log(house);
+    console.log("h>p1||h>p2");
 
     // console.log("If it makes it here, Check your rule. maybe you need an else statment");
   }
@@ -29,4 +36,4 @@ roll(6, 6);
 roll(8, 68);
 roll(22, 3);
 roll(99, 12);
-roll(98, 50);
+roll(Math.floor(Math.random() * 100), Math.floor(Math.random() * 100));
