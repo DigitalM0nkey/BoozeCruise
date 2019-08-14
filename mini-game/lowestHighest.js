6fb48ff0a4d249419fec9f4e6a27a8cd6b192d76
Terrence Kuiper <digitalm0nkey@Terrences-MacBook-Pro.local>
2019-08-13 17:50:13 -0700

    mini-game/lowestHighest: typo

--- mini-game/lowestHighest.js
+++ mini-game/lowestHighest.js
@@ -5,7 +5,7 @@ let player2 = 0;
 const roll = function numbers(player1Guess, player2Guess) {
   house = Math.floor(Math.random() * 100);
   if (player1Guess === player2Guess) {
-    console.log("House Wins! \n Player 1 guessed " + player1guess + " & Player 2 guessed " + player2Guess);
+    console.log("House Wins! \n Player 1 guessed " + player1Guess + " & Player 2 guessed " + player2Guess);
   }
   console.log(house);
 
