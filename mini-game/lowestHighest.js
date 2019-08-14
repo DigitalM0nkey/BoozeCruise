571a9ae5ea1c642e068939062fe144c5845a65e1
Terrence Kuiper <digitalm0nkey@Terrences-MacBook-Pro.local>
2019-08-13 17:49:27 -0700

    mini-game/lowestHighest: Condition if both players guess the same number.

--- mini-game/lowestHighest.js
+++ mini-game/lowestHighest.js
@@ -4,9 +4,12 @@ let player2 = 0;
 
 const roll = function numbers(player1Guess, player2Guess) {
   house = Math.floor(Math.random() * 100);
+  if (player1Guess === player2Guess) {
+    console.log("House Wins! \n Player 1 guessed " + player1guess + " & Player 2 guessed " + player2Guess);
+  }
   console.log(house);
 
 
 }
 
-roll(6, 30);
+roll(6, 6);
