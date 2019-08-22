eeff22be4d32e878391c1ea4c601b917e63f6754
Terrence Kuiper <digitalm0nkey@Terrences-MacBook-Pro.local>
2019-08-21 23:01:28 -0700

    constants/keyboards: updated inline keyboard

--- constants/keyboards.js
+++ constants/keyboards.js
@@ -231,7 +231,10 @@ module.exports = {
     for (let i = 1; i <= 20; i++) {
       keyboard.inline_keyboard.push([])
       for (let j = 1; j <= 5; j++) {
-        keyboard.inline_keyboard[keyboard.inlinekeyboard.length - 1].push(i * j)
+        keyboard.inline_keyboard[keyboard.inlinekeyboard.length - 1].push({
+          text: i * j,
+          callback_data: "Lowest Highest" + i * j
+        })
       }
     }
     return keyboard
