401f4f4121ce45939ebc8a55ef369b6e43786ef2
Terrence Kuiper <digitalm0nkey@Terrences-MacBook-Pro.local>
2019-09-04 21:44:26 -0700

    controllers/bots/BoozeCruise: moved return res.sendStatus(200);

--- controllers/bots/BoozeCruise.js
+++ controllers/bots/BoozeCruise.js
@@ -221,9 +221,9 @@ router.post('/', function (req, res, next) {
             }
           })
         }
-        //return res.sendStatus(200);
-      });
 
+      });
+    return res.sendStatus(200);
   } else if (req.body.edited_message || req.body.message.photo || req.body.message.game || req.body.message.emoji || req.body.message.voice || req.body.message.animation || req.body.message.sticker || req.body.message.reply_to_message) {
     //Ignore these messages as they're just chat interactions
     return res.sendStatus(200);
