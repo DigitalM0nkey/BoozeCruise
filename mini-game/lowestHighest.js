7b35bfdcc7fce88cb23a8047119edc4bffffc681
Terrence Kuiper <digitalm0nkey@Terrences-MacBook-Pro.local>
2019-09-11 11:23:53 -0700

    mini-game/lowestHighest: cant combine to HTML tags

--- mini-game/lowestHighest.js
+++ mini-game/lowestHighest.js
@@ -2,7 +2,7 @@ module.exports = function (house, player1, player2) {
 
   let message = "House picked: " + house + "\n" + player1.name + " guessed: " + player1.guess + "\n" + player2.name + " guessed: " + player2.guess;
 
-  let title = "<b><u>Lowest-Highest Results</u></b>\n\n";
+  let title = "<u>Lowest-Highest Results</u>\n\n";
 
   if (player1.guess === player2.guess) {
     return { message: "<b>House Wins!</b>\nPLAYER DRAW\n" + message };
