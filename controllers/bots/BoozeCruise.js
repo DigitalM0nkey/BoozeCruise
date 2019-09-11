fbae686bbdb96feaba6da6b0a1b485f9486d8632
Terrence Kuiper <digitalm0nkey@Terrences-MacBook-Pro.local>
2019-09-10 23:35:44 -0700

    controllers/bots/BoozeCruise: bug

--- controllers/bots/BoozeCruise.js
+++ controllers/bots/BoozeCruise.js
@@ -192,7 +192,7 @@ router.post('/', function (req, res, next) {
           // Start Mini-game Lowest-Highest
         } else if (data.action === 'product') {
           Product.findOne({ _id: data.product }).then(product => {
-            b.sendPhoto(req.body.callback_query.from.id, product.image, product.name);
+            b.sendPhoto(req.body.callback_query.from.id, product.image, product.description);
           })
         } else if (data.action.indexOf("LH_") === 0) {
           LowestHighest.findOne({ inProgress: true, _id: data.game.split("_")[1] }).then(function (game) {
