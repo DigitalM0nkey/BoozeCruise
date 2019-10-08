5c56c52854020a7d4e7911a9daccf122db6e59dd
Terrence Kuiper <github@terrencekuiper.com>
2019-10-07 19:30:30 -0700

    Add Screenshots for the README.md

---
+++ test/BoozeCruiseTest.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const assert = chai.assert;
+
+const BoozeCruiseTest = require('../controllers/bots/BoozeCruise');
+
+describe('nameInBoozeCruise', function() {
+
+});
