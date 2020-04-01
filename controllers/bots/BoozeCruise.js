5bfbd2f6a5ecd2e71f0fc5873b8053922c475c67
Terrence Kuiper <github@terrencekuiper.com>
2020-04-01 16:53:54 -0700

    One level deeper

--- controllers/bots/BoozeCruise.js
+++ controllers/bots/BoozeCruise.js
@@ -653,7 +653,7 @@ router.post("/", ({ body }, res, next) => {
                 let newGuests = [];
                 const embarkationBoost = perks.reduce((boost, perk) => {
                   console.log("PERK => ", perk);
-                  boost += perks.code === "PORTENTRY_EMBARKATION" ? perks.amount : 0;
+                  boost += perk.product.perk.code === "PORTENTRY_EMBARKATION" ? perk.product.perk.amount : 0;
                   console.log("BOOST => ", boost);
                   return boost > 100 ? 100 : boost;
                 }, 0);
