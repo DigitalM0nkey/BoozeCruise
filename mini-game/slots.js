2b9a34fa9fd6ffd15ba73db7cadae4cda0a1274f
Terrence Kuiper <github@terrencekuiper.com>
2020-04-18 22:06:52 -0700

    update slots.js with latest code

--- mini-game/slots.js
+++ mini-game/slots.js
@@ -1,12 +1,81 @@
-const symbols = ["🍒","⚓️","🛳","🏝","🌊","☀️"];
+const symbols = ["🍒", "🛳", "🏝", "🌊", "☀️"];
+let rolls = [
+  { symbol: "🍒", count: 0 },
+  { symbol: "🛳", count: 0 },
+  { symbol: "🏝", count: 0 },
+  { symbol: "🌊", count: 0 },
+  { symbol: "☀️", count: 0 },
+];
+let plays = 0;
 
-const slots = (bet, odds) => {
-  const house = new Array(odds);
-  for (let i = 0; i < odds; i++ ){
-      house[i] =  symbols[Math.floor(Math.random() * odds)];
-      console.log(house[i]);
+const slots = (bet) => {
+  bet = parseInt(bet, 10);
+  halfBet = Math.round(bet / 2);
+  let odds = 0;
+  if (bet <= 30) {
+    odds = 3;
+  } else if (bet <= 100) {
+    odds = Math.ceil((bet + 1) / 10);
+  } else {
+    odds = 10;
   }
-  document.getElementById("slots").innerHTML = "SLOTS";
-};
+  let balance = Number(document.getElementById("balance").innerHTML.replace(",", "")) - bet;
+  console.log("BALANCE = ", balance);
 
-slots(5, 3);
+  document.getElementById("balance").innerHTML = balance;
+  const house = new Array(odds);
+  for (let i = 0; i < odds; i++) {
+    const symbol = symbols[Math.floor(Math.random() * symbols.length)];
+    house[i] = symbol;
+    for (let roll of rolls) {
+      if (roll.symbol === symbol) {
+        roll.count++;
+        break;
+      }
+    }
+  }
+  plays++;
+  console.log(`${plays} times played`);
+  console.log(rolls);
+
+  const print = () => {
+    for (let i = 0; i < odds; i++) {
+      setTimeout(function () {
+        document.getElementById("slots").innerHTML = house.reduce(
+          (msg, symbol, j) =>
+            msg + (i >= j ? (j === house.length - 1 ? symbol : symbol + "|") : j === house.length - 1 ? "❓" : "❓|"),
+          ""
+        );
+      }, 1000 * i);
+    }
+    setTimeout(() => {
+      prizes();
+    }, (odds - 1) * 1000);
+  };
+
+  const prizes = () => {
+    i = 0;
+    prize = house.reduce((prize, symbol) => prize + (symbol === "🍒" ? halfBet : 0), 0);
+    let power = 1;
+    while (house[i] === "🍒") {
+      power += 0.1;
+      i++;
+    }
+    prize = Math.ceil(Math.pow(prize, power));
+    document.getElementById("balance").innerHTML = (balance + prize).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    console.log(`Prize: ${prize}`);
+    console.log(`Bet: ${bet}`);
+    console.log(`Half Bet: ${halfBet}`);
+    // if (house[i] === "🍒") {
+    //   document.getElementById("balance").innerHTML = balance + parseInt(bet, 10);
+    // }
+  };
+
+  const bonus = () => {
+    if (house) {
+      console.log("Hello");
+    }
+  };
+
+  print();
+};
