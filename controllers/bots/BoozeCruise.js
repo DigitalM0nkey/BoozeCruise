a3181d220e016bf240211a4cef2f3a905c8bda1a
unknown <debackerlaurent@gmail.com>
2020-04-18 21:43:40 -0700

    Log Product

--- controllers/bots/BoozeCruise.js
+++ controllers/bots/BoozeCruise.js
@@ -611,7 +611,10 @@ router.post("/", ({ body }, res, next) => {
               .populate("products.product")
               .then((ship) => {
                 const perks = ship.products.filter(
-                  (product) => product.code.indexOf("PORTENTRY") === 0 && product.expiry > moment()
+                  (product) => {
+                    console.log(product);
+                    return product.code.indexOf("PORTENTRY") === 0 && product.expiry > moment();
+                  }
                 );
                 port.ships.push(ship._id);
                 port.save();
