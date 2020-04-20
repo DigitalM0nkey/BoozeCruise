5fba7ba0500646aa1b86a6c47dab430e2ee708dc
Terrence Kuiper <github@terrencekuiper.com>
2020-04-20 12:21:19 -0700

    revert till after lunch

--- mini-game/slots.js
+++ mini-game/slots.js
@@ -71,10 +71,10 @@ module.exports = (ship, bet) => {
     console.log(rolls);
 
     const print = () => {
-      b.sendMessage(ship.id, "❓|❓|❓", "");
+      // b.sendMessage(ship.id, "❓|❓|❓", "");
       for (let i = 0; i < odds; i++) {
         setTimeout(function () {
-          b.editMessageText(
+          b.sendMessage(
             ship.id,
             house.reduce(
               (msg, symbol, j) =>
