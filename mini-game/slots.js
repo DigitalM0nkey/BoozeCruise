53bf8aa4d7eb2eaaed526d7cbed99672a3aaa7d2
Terrence Kuiper <github@terrencekuiper.com>
2020-04-20 10:03:28 -0700

    Convert odd into switch

--- mini-game/slots.js
+++ mini-game/slots.js
@@ -15,13 +15,45 @@ module.exports = (ship, bet) => {
     bet = parseInt(bet, 10);
     halfBet = Math.round(bet / 2);
     let odds = 0;
-    if (bet <= 91) {
-      odds = 3;
-    } else if (bet <= 10) {
-      odds = Math.ceil((bet - 1) / 10);
-    } else {
-      odds = 10;
+    switch (bet) {
+      case 10:
+      case 20:
+      case 30:
+        odds = 10;
+        break;
+      case 40:
+        odds = 9;
+        break;
+      case 50:
+        odds = 8;
+        break;
+      case 60:
+        odds = 7;
+        break;
+      case 70:
+        odds = 6;
+        break;
+      case 80:
+        odds = 5;
+        break;
+      case 90:
+        odds = 4;
+        break;
+      case 100:
+        odds = 3;
+        break;
+      default:
+        odds = 10;
     }
+
+    // if (bet >= 90) {
+    //   odds = 3;
+    // } else if (bet >= 30) {
+    //   odds = Math.ceil((bet + 1) / 10);
+    // } else {
+    //   odds = 10;
+    // }
+    console.log("ODDS =>", odds);
 
     const house = new Array(odds);
     for (let i = 0; i < odds; i++) {
