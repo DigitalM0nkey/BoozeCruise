a03eeeb81b0eda928d375014d7f6b4ee9942f60c
Terrence Kuiper <github@terrencekuiper.com>
2020-04-24 12:48:59 -0700

    Update slots message

--- controllers/bots/BoozeCruise.js
+++ controllers/bots/BoozeCruise.js
@@ -318,7 +318,7 @@ router.post("/", ({ body }, res, next) => {
           } else if (body.message.text == `${emoji.slots} Slots ${emoji.slots}`) {
             log(player, "Playing the slots");
             // console.log(keyboards.numbers("", "SL"));
-            b.sendKeyboard(ship.id, "Place your bet", keyboards.slots("", "SL"));
+            b.sendKeyboard(ship.id, `Place your bet\n🍒=✅`, keyboards.slots("", "SL"));
           } else if (body.message.text == `/beta`) {
             b.sendKeyboard(ship.id, "Your testing beta features! Things may break", keyboards.beta);
             log(player, "Testing beta features");
