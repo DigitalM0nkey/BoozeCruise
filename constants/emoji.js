65d89d15ccfdd50c319acbd6a4326563296605af
Terrence Kuiper <github@terrencekuiper.com>
2020-04-26 00:56:29 -0700

    remove leading and trailing spaces from emoji.korona

--- constants/emoji.js
+++ constants/emoji.js
@@ -5,7 +5,7 @@ module.exports = {
   fireworks: " \ud83c\udf87 ",
   moneyBag: " \ud83d\udcb0 ",
   bug: " \ud83d\udc1b ",
-  korona: " \u24C0 ",
+  korona: "\u24C0",
   mainMenu: "\u2630 Main Menu \u2630",
   navigation: " \ud83d\uddfa Navigation \ud83d\uddfa ",
   upDown: " \u2195 ",
