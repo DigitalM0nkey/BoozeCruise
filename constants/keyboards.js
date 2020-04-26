6568b7cbbaf367e03dcd781833c4167e9c30cec6
Terrence Kuiper <github@terrencekuiper.com>
2020-04-25 21:31:51 -0700

    Make bet keyboard horizontal

--- constants/keyboards.js
+++ constants/keyboards.js
@@ -386,22 +386,24 @@ module.exports = {
     resize_keyboard: true,
   },
   mixology: {
-    inline_keyboard: [[{
-      text: "Mixology",
-      callback_data: JSON.stringify({ action: `mixology` })
-    }]]
+    inline_keyboard: [
+      [
+        {
+          text: "Mixology",
+          callback_data: JSON.stringify({ action: `mixology` }),
+        },
+      ],
+    ],
   },
   slots: function (gameId, type) {
     let keyboard = {
-      inline_keyboard: [],
+      inline_keyboard: [[]],
     };
     for (let i = 20; i <= 100; i += 20) {
-      keyboard.inline_keyboard.push([
-        {
-          text: i,
-          callback_data: JSON.stringify({ action: `${type}_${gameId}`, num: i }),
-        },
-      ]);
+      keyboard.inline_keyboard.push({
+        text: i,
+        callback_data: JSON.stringify({ action: `${type}_${gameId}`, num: i }),
+      });
     }
     return keyboard;
   },
