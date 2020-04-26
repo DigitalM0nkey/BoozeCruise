f2917ddc35374dec3f61364df682c6b123582f16
Terrence Kuiper <github@terrencekuiper.com>
2020-04-25 21:52:55 -0700

    revert

--- constants/keyboards.js
+++ constants/keyboards.js
@@ -397,12 +397,7 @@ module.exports = {
   },
   slots: function (gameId, type) {
     let keyboard = {
-      inline_keyboard: [
-        {
-          text: "Stats for nerds",
-          callback_data: JSON.stringify({ action: `slotStats` }),
-        },
-      ],
+      inline_keyboard: [],
     };
     for (let i = 20; i <= 100; i += 20) {
       keyboard.inline_keyboard.push([
