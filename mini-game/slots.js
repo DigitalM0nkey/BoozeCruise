77f9f0859c17fabda7956c752538785a65766723
Terrence Kuiper <github@terrencekuiper.com>
2020-05-02 23:47:15 -0700

    debugging

--- mini-game/slots.js
+++ mini-game/slots.js
@@ -25,162 +25,160 @@ let highestPower = 0;
 
 const slots = (ship, bet, messageId) => {
   console.log(`ship => ${ship}`);
-  Ship.findOne({ id: ship.from.id }).then(function (ship) {
-    ship.purse.balance -= bet;
-    ship.save();
-    return new Promise(function (resolve, reject) {
-      bet = parseInt(bet, 10);
-      halfBet = Math.round(bet / 2);
-      let odds = 0;
-      switch (bet) {
-        case 10:
-        case 20:
-        case 30:
-          odds = 10;
+  ship.purse.balance -= bet;
+  ship.save();
+  return new Promise(function (resolve, reject) {
+    bet = parseInt(bet, 10);
+    halfBet = Math.round(bet / 2);
+    let odds = 0;
+    switch (bet) {
+      case 10:
+      case 20:
+      case 30:
+        odds = 10;
+        break;
+      case 40:
+        odds = 9;
+        break;
+      case 50:
+        odds = 8;
+        break;
+      case 60:
+        odds = 7;
+        break;
+      case 70:
+        odds = 6;
+        break;
+      case 80:
+        odds = 5;
+        break;
+      case 90:
+        odds = 4;
+        break;
+      case 100:
+        odds = 3;
+        break;
+      default:
+        odds = 10;
+    }
+
+    // if (bet >= 90) {
+    //   odds = 3;
+    // } else if (bet >= 30) {
+    //   odds = Math.ceil((bet + 1) / 10);
+    // } else {
+    //   odds = 10;
+    // }
+    console.log("ODDS =>", odds);
+
+    const house = new Array(odds);
+    for (let i = 0; i < odds; i++) {
+      const symbol = symbols[Math.floor(Math.random() * symbols.length)];
+      house[i] = symbol;
+      for (let roll of rolls) {
+        if (roll.symbol === symbol) {
+          roll.count++;
           break;
-        case 40:
-          odds = 9;
-          break;
-        case 50:
-          odds = 8;
-          break;
-        case 60:
-          odds = 7;
-          break;
-        case 70:
-          odds = 6;
-          break;
-        case 80:
-          odds = 5;
-          break;
-        case 90:
-          odds = 4;
-          break;
-        case 100:
-          odds = 3;
-          break;
-        default:
-          odds = 10;
+        }
       }
+    }
+    plays++;
+    console.log(`${plays} times played`);
+    console.log(rolls);
 
-      // if (bet >= 90) {
-      //   odds = 3;
-      // } else if (bet >= 30) {
-      //   odds = Math.ceil((bet + 1) / 10);
-      // } else {
-      //   odds = 10;
-      // }
-      console.log("ODDS =>", odds);
-
-      const house = new Array(odds);
+    const print = () => {
       for (let i = 0; i < odds; i++) {
-        const symbol = symbols[Math.floor(Math.random() * symbols.length)];
-        house[i] = symbol;
-        for (let roll of rolls) {
-          if (roll.symbol === symbol) {
-            roll.count++;
-            break;
-          }
+        setTimeout(function () {
+          b.editMessageText(
+            ship.id,
+            messageId,
+            house.reduce(
+              (msg, symbol, j) =>
+                msg +
+                (i >= j ? (j === house.length - 1 ? symbol : symbol + "|") : j === house.length - 1 ? "❓" : "❓|"),
+              ""
+            )
+          );
+        }, 1000 * i);
+      }
+      setTimeout(() => {
+        prizes();
+      }, odds * 1000);
+    };
+
+    const prizes = () => {
+      let i = 0;
+      let prize = house.reduce((prize, symbol) => prize + (symbol === "🍒" ? halfBet : 0), 0);
+      let power = 1;
+      let jackpot = 0;
+      let bonus = 0;
+
+      console.log("JACKPOT ??? => ", checkJackpot(house));
+
+      if (checkJackpot(house)) {
+        jackpot = Math.pow(bet, 1 + odds / 5);
+        if (largestJackpot.amount < jackpot) {
+          largestJackpot.amount = jackpot;
+          largestJackpot.winningSymbols = house;
         }
       }
-      plays++;
-      console.log(`${plays} times played`);
-      console.log(rolls);
+      if (trifector(house)) {
+        bonus = bet * 1.5 * trifector(house);
+      }
+      while (house[i] === "🍒") {
+        power += 0.1;
+        i++;
+      }
 
-      const print = () => {
-        for (let i = 0; i < odds; i++) {
-          setTimeout(function () {
-            b.editMessageText(
-              ship.id,
-              messageId,
-              house.reduce(
-                (msg, symbol, j) =>
-                  msg +
-                  (i >= j ? (j === house.length - 1 ? symbol : symbol + "|") : j === house.length - 1 ? "❓" : "❓|"),
-                ""
-              )
-            );
-          }, 1000 * i);
+      prize = Math.ceil(Math.pow(prize, power) + jackpot + bonus);
+      console.log(`Prize: ${prize}`);
+      console.log(`Jackpot: ${jackpot}`);
+      console.log(`Amount of trifectors: ${trifector(house)}`);
+      console.log(`Bonus: ${bonus}`);
+      console.log(`Bet: ${bet}`);
+      console.log(`Power ${power}`);
+
+      if (power > highestPower) {
+        highestPower = power;
+      }
+
+      amountBet += bet;
+      amountWon += prize;
+      amountOftrifectors += trifector(house);
+
+      resolve(prize);
+      // if (house[i] === "🍒") {
+      //   document.getElementById("balance").innerHTML = balance + parseInt(bet, 10);
+      // }
+    };
+
+    const trifector = (slots) => {
+      let telly = 0;
+      for (let i = 0; i < slots.length - 2; i++) {
+        if (slots[i] === slots[i + 1] && slots[i] === slots[i + 2]) {
+          telly++;
+          i += 2;
         }
-        setTimeout(() => {
-          prizes();
-        }, odds * 1000);
+      }
+      return telly;
+    };
+    //const checkJackpot = slots => slots.every(symbol => symbol === slots[0]);
+
+    const checkJackpot = (currentValue) => {
+      console.log(currentValue);
+      const equal = (value) => {
+        console.log(value === currentValue[0]);
+        return value === currentValue[0];
       };
+      if (currentValue.every(equal)) {
+        console.log("Jackpot");
+        return true;
+      } else {
+        return false;
+      }
+    };
 
-      const prizes = () => {
-        let i = 0;
-        let prize = house.reduce((prize, symbol) => prize + (symbol === "🍒" ? halfBet : 0), 0);
-        let power = 1;
-        let jackpot = 0;
-        let bonus = 0;
-
-        console.log("JACKPOT ??? => ", checkJackpot(house));
-
-        if (checkJackpot(house)) {
-          jackpot = Math.pow(bet, 1 + odds / 5);
-          if (largestJackpot.amount < jackpot) {
-            largestJackpot.amount = jackpot;
-            largestJackpot.winningSymbols = house;
-          }
-        }
-        if (trifector(house)) {
-          bonus = bet * 1.5 * trifector(house);
-        }
-        while (house[i] === "🍒") {
-          power += 0.1;
-          i++;
-        }
-
-        prize = Math.ceil(Math.pow(prize, power) + jackpot + bonus);
-        console.log(`Prize: ${prize}`);
-        console.log(`Jackpot: ${jackpot}`);
-        console.log(`Amount of trifectors: ${trifector(house)}`);
-        console.log(`Bonus: ${bonus}`);
-        console.log(`Bet: ${bet}`);
-        console.log(`Power ${power}`);
-
-        if (power > highestPower) {
-          highestPower = power;
-        }
-
-        amountBet += bet;
-        amountWon += prize;
-        amountOftrifectors += trifector(house);
-
-        resolve(prize);
-        // if (house[i] === "🍒") {
-        //   document.getElementById("balance").innerHTML = balance + parseInt(bet, 10);
-        // }
-      };
-
-      const trifector = (slots) => {
-        let telly = 0;
-        for (let i = 0; i < slots.length - 2; i++) {
-          if (slots[i] === slots[i + 1] && slots[i] === slots[i + 2]) {
-            telly++;
-            i += 2;
-          }
-        }
-        return telly;
-      };
-      //const checkJackpot = slots => slots.every(symbol => symbol === slots[0]);
-
-      const checkJackpot = (currentValue) => {
-        console.log(currentValue);
-        const equal = (value) => {
-          console.log(value === currentValue[0]);
-          return value === currentValue[0];
-        };
-        if (currentValue.every(equal)) {
-          console.log("Jackpot");
-          return true;
-        } else {
-          return false;
-        }
-      };
-
-      print();
-    });
+    print();
   });
 };
 
