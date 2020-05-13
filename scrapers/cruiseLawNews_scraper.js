19a2d7f15648099e298fe95cb8b071d1ddbbdf83
unknown <debackerlaurent@gmail.com>
2020-05-12 23:27:41 -0700

    Try promisified article scraper

--- scrapers/cruiseLawNews_scraper.js
+++ scrapers/cruiseLawNews_scraper.js
@@ -1,37 +1,43 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
-const url = "https://www.cruiselawnews.com/";
 const articles = [];
 
-const cruiseLawNewsArticle = (url) =>
+const cruiseLawNewsArticle = url => new Promise(function(resolve, reject) {
   axios
-  .get(url)
-  .then((response) => {
-    const $ = cheerio.load(response.data);
-    let urls = [];
-    for (let i = 1; i < 11; i++) {
-      urls.push($("h1 > a")[i].attribs.href);
-    }
-    return urls;
-  })
-  // .then((urls) => {
-  //   urls.forEach((url) => {
-  //     axios.get(url).then((response) => {
-  //       const $ = cheerio.load(response.data);
-  //       const article = {
-  //         image: $(".lxb_af-featured_image-get-img").attr("src"),
-  //         title: $(".lxb_af-template_tags-get_post_title").text(),
-  //         body: $(".lxb_af-post_content").text(),
-  //       };
-  //       articles.push(article);
-  //       //console.log("ARTICLES => ", articles);
-  //     });
-  //   });
-  // })
-  .catch(function(err) {
-    console.log("SCRAPER ERROR =>", err);
-  });
+    .get(url)
+    .then(response => {
+      const $ = cheerio.load(response.data);
+      let urls = [];
+      for (let i = 1; i < 11; i++) {
+        urls.push($("h1 > a")[i].attribs.href);
+      }
+      return urls;
+    })
+    .then(urls => {
+      Promise.all(urls.map(url =>
+          axios.get(url)
+        ))
+        .then(responses => {
+          resolve(responses.map(response => {
+            const $ = cheerio.load(response.data);
+            return {
+              image: $(".lxb_af-featured_image-get-img").attr("src"),
+              title: $(".lxb_af-template_tags-get_post_title").text(),
+              body: $(".lxb_af-post_content").text(),
+            };
+          }));
+        });
 
+    })
+    .catch(err => {
+      console.log("SCRAPER ERROR =>", err);
+      reject(err);
+    });
+});
+
+cruiseLawNewsArticle("https://www.cruiselawnews.com/").then(articles => console.log(articles), console.error);
+
+/*
 const clnArticles = (urls) =>
   axios
   .get(urls)
@@ -57,6 +63,7 @@ axios.all([cruiseLawNewsArticle("https://www.cruiselawnews.com/"), clnArticles("
     // Both requests are now complete
   })
 );
+*/
 // console.log(cruiseLawNewsArticle(url));
 
 // console.log(clnArticles(cruiseLawNewsArticle("https://www.cruiselawnews.com/")));
