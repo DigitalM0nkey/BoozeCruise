30f7a3bbab77c46eb7c438fe7dfa6c988c91f2ce
unknown <debackerlaurent@gmail.com>
2020-05-12 22:02:48 -0700

    More logs

--- bots/telegram.js
+++ bots/telegram.js
@@ -122,7 +122,8 @@ function TelegramBot() {
       )}`;
       request(encodeURI(url), (error, r, body) => {
         const response = JSON.parse(body).result;
-        //console.log(response);
+        console.log(keyboard);
+        console.log(JSON.parse(body));
         if (error) reject(error);
         if (!response) reject('No response');
         resolve(`Keyboard sent to ${channel}`);
