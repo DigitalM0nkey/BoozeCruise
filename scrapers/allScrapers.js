e96e6c324057c57db893a77a753de9c43da638f8
Terrence Kuiper <github@terrencekuiper.com>
2020-05-14 11:36:48 -0700

    Add logic for picking random article from all articles

--- scrapers/allScrapers.js
+++ scrapers/allScrapers.js
@@ -1,6 +1,7 @@
 let articles = {};
 const cruiseLawNewsArticles = require("../scrapers/cruiseLawNews_scraper");
 const iLikeCruiseShipsArticles = require("../scrapers/iLikeCruiseShips_scraper");
+const _ = require("underscore");
 
 async function runAllScrapers() {
   articles.iLikeCruiseShips = await iLikeCruiseShipsArticles();
@@ -8,4 +9,12 @@ async function runAllScrapers() {
   return articles;
 }
 
+const cleanData = () => {
+  runAllScrapers().then((result) => {
+    let source = _.sample(Object.keys(result));
+    console.log(result[source][_.random(0, 10)].title);
+  });
+};
+
+cleanData();
 exports.runAllScrapers = runAllScrapers;
