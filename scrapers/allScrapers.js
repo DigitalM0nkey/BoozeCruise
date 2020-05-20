f08dd322347511e256110a39687583e502221424
Terrence Kuiper <github@terrencekuiper.com>
2020-05-20 00:44:35 -0700

    refactor

--- scrapers/allScrapers.js
+++ scrapers/allScrapers.js
@@ -6,10 +6,12 @@ const _ = require("underscore");
 
 async function runAllScrapers() {
   try {
-    articles.push(await cruiseIndustryNewsArticles());
+    //articles = await cruiseIndustryNewsArticles();
     //articles.iLikeCruiseShips = await iLikeCruiseShipsArticles();
-    articles.push(await cruiseLawNewsArticles());
-    return articles;
+    // const newArticle = (await cruiseIndustryNewsArticles()).concat(await cruiseLawNewsArticles());
+    //console.log(newArticle);
+
+    return (await cruiseIndustryNewsArticles()).concat(await cruiseLawNewsArticles());
   } catch (err) {
     // catches errors both in fetch and response.json
     console.error(err);
@@ -19,7 +21,7 @@ async function runAllScrapers() {
 const cleanData = async () => {
   try {
     const result = await runAllScrapers();
-    console.log("result --------->", result);
+    // console.log("result --------->", result);
     // const source = _.sample(Object.keys(result));
     let sources = Object.keys(result);
     console.log("SOURCES =>", sources);
@@ -32,14 +34,15 @@ const cleanData = async () => {
     //const randNum = _.random(0, sources.length - 1);
     // console.log(randNum);
 
-    console.log("result[SOURCE] --------->", result[`${source}`][1].source);
+    // console.log("result[SOURCE] --------->", result[`${source}`][1].source);
 
-    const onlyShort = result[`${source}`].filter((article) => article.body.length < 4050);
+    const onlyShort = result.filter((article) => article.body.length < 4050);
     console.log("ONLY SHORT", onlyShort.length);
 
     const withPhotos = onlyShort.filter((article) => article.image);
     console.log("WITH PHOTO", withPhotos.length);
     const randomArticle = withPhotos[_.random(0, withPhotos.length - 1)];
+    console.log("RANDOM ARTICLE", randomArticle);
 
     //console.log(`source => ${source}.com | title => ${randomArticle.title} | length => ${randomArticle.body.length}`);
     return randomArticle;
