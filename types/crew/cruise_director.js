3e338a0eff4cdf9fb47e56b8efec24656c796eba
Terrence Kuiper <github@terrencekuiper.com>
2020-08-31 23:30:49 -0700

    Are you there?

--- types/crew/cruise_director.js
+++ types/crew/cruise_director.js
@@ -3,14 +3,14 @@ module.exports = {
     if (!input) return `Hi, I'm your Cruise Director, What do you need help with today?`;
     else if (input.checkString("pool")) return `The pool is that-a-way 💪 👉`;
     else if (input.checkString("name") && input.checkString("your")) return `My name is Jared Burns`;
+    else if (input.checkString("you") && input.checkString("are") && input.checkString("there"))
+      return `Sure am. What's up`;
     else if (input.checkString("fuck") || input.checkString("fucking"))
       return [
         `Language....Please....There are children onboard.`,
         `Holy shit... No fucking way!`,
         `Oh, Donkey balls`,
       ].getRandom();
-    else {
-      return "";
-    }
+    else return "";
   },
 };
