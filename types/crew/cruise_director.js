04c8675d5c9940784bd329aad6a54fe827210f7f
Terrence Kuiper <github@terrencekuiper.com>
2020-08-31 23:40:21 -0700

    Bloody semicolon

--- types/crew/cruise_director.js
+++ types/crew/cruise_director.js
@@ -4,9 +4,9 @@ module.exports = {
     else if (input.checkString("pool")) return `The pool is that-a-way 💪 👉`;
     else if (input.checkString("hello") || input.checkString("hi")) return `👋`;
     else if (input.checkString("name") && input.checkString("your")) return `My name is Jared Burns`;
-    else if (input.checkString("you") && input.checkString("are") && input.checkString("there"))
+    else if (input.checkString("you") && input.checkString("are") && input.checkString("there"));
     else if (input.checkString("how") && input.checkString("are") && input.checkString("you"))
-    return `I live and work on a cruise ship... Life is amazing!`;
+      return `I live and work on a cruise ship... Life is amazing!`;
     else if (input.checkString("fuck") || input.checkString("fucking"))
       return [
         `Language....Please....There are children onboard.`,
