5e549c67f143b4628bd8721440e8f142facb3b51
Terrence Kuiper <github@terrencekuiper.com>
2020-08-31 22:58:57 -0700

    Don't swear

--- types/crew/cruise_director.js
+++ types/crew/cruise_director.js
@@ -4,5 +4,7 @@ module.exports = {
       return `Hi, I'm your Cruise Director, What do you need help with today?`;
     } else if (input.checkString("pool")) return `The pool is that-a-way 💪 👉`;
     else if (input.checkString("name") && input.checkString("your")) return `My name is Jared Burns`;
+    else if (input.checkString("fuck") || input.checkString("fucking"))
+      return `Language....Please....There are children onboard.`;
   },
 };
