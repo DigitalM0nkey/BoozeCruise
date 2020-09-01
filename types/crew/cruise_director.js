c1576b75b372eb8362660e74c28ab4d5c5a709b9
Terrence Kuiper <github@terrencekuiper.com>
2020-08-31 23:32:14 -0700

    hello

--- types/crew/cruise_director.js
+++ types/crew/cruise_director.js
@@ -2,6 +2,7 @@ module.exports = {
   conversation: (input) => {
     if (!input) return `Hi, I'm your Cruise Director, What do you need help with today?`;
     else if (input.checkString("pool")) return `The pool is that-a-way 💪 👉`;
+    else if (input.checkString("Hello") || input.checkString("Hi")) return `👋`;
     else if (input.checkString("name") && input.checkString("your")) return `My name is Jared Burns`;
     else if (input.checkString("you") && input.checkString("are") && input.checkString("there"))
       return `Sure am. What's up`;
