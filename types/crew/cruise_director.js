d5325047402b76eaa671fda29dec92ee26fac0e3
Terrence Kuiper <github@terrencekuiper.com>
2020-08-31 22:07:44 -0700

    forgot to return

--- types/crew/cruise_director.js
+++ types/crew/cruise_director.js
@@ -1,7 +1,7 @@
 module.exports = {
   conversation: (input) => {
     if (!input) {
-      `Hi, I'm your Cruise Director, What do you need help with today?`;
-    } else if (input) `You said ${input}`;
+      return `Hi, I'm your Cruise Director, What do you need help with today?`;
+    } else if (input) return `You said ${input}`;
   },
 };
