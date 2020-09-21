777f8d198747e47000d4852173f099a4c93591a5
Terrence Kuiper <github@terrencekuiper.com>
2020-09-21 05:23:10 +0000

    remove logs in artsy

--- pirates/art.js
+++ pirates/art.js
@@ -1,25 +1,27 @@
-const axios = require('axios');
-const apiUrl = 'https://api.artsy.net/api/tokens/xapp_token';
-let xappToken = '';
+const axios = require("axios");
+const apiUrl = "https://api.artsy.net/api/tokens/xapp_token";
+let xappToken = "";
 
-axios.post(apiUrl, {
+axios
+  .post(apiUrl, {
     client_id: process.env.ARTSY_CLIENT_ID,
-    client_secret: process.env.ARTSY_CLIENT_SECRET
+    client_secret: process.env.ARTSY_CLIENT_SECRET,
   })
-  .then(res => {
+  .then((res) => {
     xappToken = res.data.token;
-    axios.get('https://api.artsy.net/api/artworks?sample=1', {
+    axios
+      .get("https://api.artsy.net/api/artworks?sample=1", {
         headers: {
-          'X-Xapp-Token': xappToken //the token is a variable which holds the token
-        }
+          "X-Xapp-Token": xappToken, //the token is a variable which holds the token
+        },
       })
-      .then(function(res) {
-        console.log(res.data);
+      .then(function (res) {
+        // console.log(res.data);
       })
-      .catch(function(err) {
-        console.error(err);
+      .catch(function (err) {
+        // console.error(err);
       });
   })
-  .catch(function(err) {
-    console.error(err);
+  .catch(function (err) {
+    // console.error(err);
   });
