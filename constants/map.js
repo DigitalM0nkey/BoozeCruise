820851c5202eb9074659515a3f0dd903a13fb8dd
Terrence Kuiper <github@terrencekuiper.com>
2020-09-23 08:17:06 +0000

    add ship map

---
+++ constants/map.js
@@ -0,0 +1,9 @@
+const map = {
+  Casino: ["slots", "lowestHighest"],
+  Bridge: ["logs", "navigation"],
+  Lounge: ["cocktail"],
+  Library: ["news"],
+  Purcer: ["deposit", "withdraw"],
+  Shop: ["products"],
+};
+console.log(map.Shop);
