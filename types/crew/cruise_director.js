ce8dd04b107903873597253ac0542fa0518b614e
Terrence Kuiper <github@terrencekuiper.com>
2020-09-23 07:17:28 +0000

    Shortcut for input.checkString

--- types/crew/cruise_director.js
+++ types/crew/cruise_director.js
@@ -1,13 +1,14 @@
 module.exports = {
   conversation: (input) => {
+    const trigger = input.checkString;
     if (!input) return `Hi, I'm your Cruise Director, What do you need help with today?`;
-    else if (input.checkString("pool")) return `The pool is that-a-way 💪 👉`;
-    else if (input.checkString("hello") || input.checkString("hi")) return `👋`;
-    else if (input.checkString("name") && input.checkString("your")) return `My name is Jared Burns`;
-    else if (input.checkString("you") && input.checkString("are") && input.checkString("there"));
-    else if (input.checkString("how") && input.checkString("are") && input.checkString("you"))
+    else if (trigger("pool")) return `The pool is that-a-way 💪 👉`;
+    else if (trigger("hello") || trigger("hi")) return `👋`;
+    else if (trigger("name") && trigger("your")) return `My name is Jared Burns`;
+    else if (trigger("you") && trigger("are") && trigger("there"));
+    else if (trigger("how") && trigger("are") && trigger("you"))
       return `I live and work on a cruise ship... Life is amazing!`;
-    else if (input.checkString("fuck") || input.checkString("fucking"))
+    else if (trigger("fuck") || trigger("fucking"))
       return [
         `Language....Please....There are children onboard.`,
         `Holy shit... No fucking way!`,
